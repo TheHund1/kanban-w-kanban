429cec939714a0b94092cb9205bd48819dc7f1d8
ravvck <>
2025-01-06 11:20:42 +0100

    Add delete task functionality

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,8 +1,10 @@
-export default function Task({ task, onMove }) {
+export default function Task({ task, onMove, onDelete }) {
   return (
     <div className="task">
       <p>{task.text}</p>
+     
       <small>By: {task.userEmail}</small>
+      <div>
       {task.status === 'todo' && (
         <button onClick={() => onMove(task.id, 'inProgress')}>→</button>
       )}
@@ -15,6 +17,10 @@ export default function Task({ task, onMove }) {
       {task.status === 'done' && (
         <button onClick={() => onMove(task.id, 'inProgress')}>←</button>
       )}
+      <button onClick={() => onDelete(task.id)} className="delete-task-button"> DELETE </button>
+      </div>
     </div>
+
+  
   );
 }
